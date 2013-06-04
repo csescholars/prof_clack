a716e676dd0dbcffe6e8ab47580d3eda2f8d12f2
Jeff Sallans <jeffsallans@gmail.com>
2013-06-03 21:52:44 -0400

    sexy nav bar

--- includes/nav.php
+++ includes/nav.php
@@ -1,23 +1,26 @@
-<div class='header'>
-  <div class='image'>
-    <img src="images/gvsu_predental_logo_trans.png">
-  </div>
+<div class='entire_nav_bar'>
   <div class='title'>
-    <h1>
-      Clack Website
-    </h1>
+    <a href='index.html'>
+      <h1>
+        Clack Website
+      </h1>
+    </a>
   </div>
-  <br />
-  <div class='title_description'>
-    Association for Grand Valley State University students interested in pursuing a career in dentistry.
-  </div> 
+  <div class='nav_bar'>
+    <div class='tabs'>
+      <a href='about.html'><span class="tab_item about">People</span></a>
+      <a href='join.html'><span class="tab_item join selected">Research</span></a>
+      <a href='calendar.html'><span class="tab_item calendar">Publications</span></a>
+      <a href='members.html'><span class="tab_item members">Photos</span></a>
+      <a href='connect.html'><span class="tab_item connect">Links</span></a>
+    </div>
+  </div>
 </div>
-<div class='tabs'>
-  <a href='index.html'><span class="tab_item home">Home</span></a>
-  <a href='about.html'><span class="tab_item about">About</span></a>
-  <a href='join.html'><span class="tab_item join">Join</span></a>
-  <a href='calendar.html'><span class="tab_item calendar">Calendar</span></a>
-  <a href='members.html'><span class="tab_item members">Members</span></a>
-  <a href='connect.html'><span class="tab_item connect">Connect</span></a>
-  <span class="tab_item_right"><b>Next Meeting:</b> <span class='cushycms'>9pm @ Kirkoff<span></span>
+<div class="nav_bar_finish">
 </div>
+<div class="nav_diag">
+</div>
+<br />
+<br />
+<br />
+<br />
