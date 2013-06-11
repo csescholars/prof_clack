1f341d1f425c2caa7388ca35be7ef1be59e2a70a
Jeff Sallans <jeffsallans@gmail.com>
2013-06-10 20:42:18 -0400

    people page

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
 	<p class="pull-right"><a href="#">Back to top</a></p>
-	<p>&copy; 2013 Dr. Herek Clack &middot; <a href="members.php">Contact</a></p>
+	<p>&copy; 2013 Dr. Herek Clack &middot; <a href="people.php">Contact</a></p>
 </div>  
