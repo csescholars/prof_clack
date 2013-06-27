7a116bbb6c443ab47d9f467c5b23630ec502a78d
Benjamin <benrm@umich.edu>
2013-06-26 22:27:01 -0400

    Pared down update

--- db_access/update_research.php
+++ db_access/update_research.php
@@ -10,45 +10,6 @@ if ($db->connect_error) {
 $new_title = $_POST['new_title'];
 $old_title = $_POST['old_title'];
 
-if(strncmp($new_title, $old_title, 200))
-{
-	$stmt = $db->prepare("SELECT COUNT(*) FROM research WHERE title=?");
-	$stmt->bind_param('s', $title);
-	
-	$title = $new_title;
-	
-	if($stmt->execute() == false)
-	{
-		printf("Statement failed\n");
-		exit();
-	}
-	$result = $stmt->get_result();
-	$row = $result->fetch_row();
-	if($row[0] != 0)
-	{
-		printf("Invalid new title: in use\n");
-		exit();
-	}
-	$row = array();
-	$result->free();
-	
-	$title = $old_title;
-	if($stmt->execute() == false)
-	{
-		printf("Statement failed\n");
-		exit();
-	}
-	$result = $stmt->get_result();
-	$row = $result->fetch_row();
-	if($row[0] != 1)
-	{
-		printf("Invalid old title: no entry exists\n"
-		exit();
-	}
-	$result->free();
-	$stmt->close();
-}
-
 $stmt = $db->prepare("UPDATE research SET title=?, paragraph=? WHERE title=?");
 $stmt->bind_param('sss', $new_title, $paragraph, $old_title);
 
